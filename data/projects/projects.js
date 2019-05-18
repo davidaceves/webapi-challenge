6021fd182c42ce6e6f656ad4cef4d4c339a88dff
David <digital0david@gmail.com>
2019-05-18 13:17:21 -0400

    connections added

---
+++ data/projects/projects.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const router = express.Router();
+
+const Projects = require('../helpers/projectModel.js');
+const Actions = require('../helpers/actionModel.js');
+
+module.exports = router;
