0c409e17930f990f32d24c0b7507d18494ec9e2e
David <digital0david@gmail.com>
2019-05-18 14:05:12 -0400

    action get

--- data/projects/actions.js
+++ data/projects/actions.js
@@ -4,4 +4,23 @@ const router = express.Router();
 
 const Actions = require('../helpers/actionModel.js');
 
+router.get('/:id', async (req, res) => {
+    try {
+        const actions = await Actions.get(req.params.id);
+
+        if (actions) {
+            res.status(200).json(actions);
+        } else {
+            res.status(404).json({
+                message: "Action could not be found"
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Error retrieving actions'
+        });
+    }
+});
+
 module.exports = router;
