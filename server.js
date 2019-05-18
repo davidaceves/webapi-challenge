78a7f3148aa422b4d5cc7aaa151c106c6b8867b9
David <digital0david@gmail.com>
2019-05-18 13:03:09 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
-
-const ProjectRouter = require('./data/projects/projects.js');
-
 const server = express();
 
+const ProjectRouter = require('./data/projects/projects.js');
+const ActionRouter = require('./data/projects/actions.js');
+
+server.use(express.json());
+
 server.use('/api/projects', ProjectRouter);
+server.use('/api/actions', ActionRouter)
 
 server.get('/', (req, res) => {
     res.send(`<h1>Welcome</h1>`)
